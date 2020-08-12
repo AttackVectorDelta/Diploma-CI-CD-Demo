d8ed4550d85fa2ff0cb48c11775bf8ba1c1c343e
AttackVectorDelta <nejc.ackun@gmail.com>
2020-08-13 01:40:09 +0200

    Added Vector4f class and full suite of tests to the project

---
+++ Vector4f.js
@@ -0,0 +1,124 @@
+class Vector4f {
+    
+    constructor (x, y, z, h){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.h = h;
+    }
+
+
+    static negate(input){
+        if(input instanceof Vector4f != true){
+            throw new Error("Parameter is not a vector!");
+        }
+
+        return new Vector4f(-input.x, -input.y, -input.z, input.h);
+    }
+
+    static add(input1, input2) {
+        if(input1 instanceof Vector4f != true || input2 instanceof Vector4f != true){
+            throw "Parameters are not vectors!"
+        }
+
+        return new Vector4f(
+            (input1.x+input2.x), 
+            (input1.y+input2.y), 
+            (input1.z+input2.z),
+            input1.h
+        );
+    }
+
+    static scalarProduct(input1, input2){
+        if(typeof input1 != "number" || input2 instanceof Vector4f != true){
+            throw "Check parameter types!"
+        }
+        return new Vector4f(input2.x*input1, input2.y*input1, input2.z*input1, input2.h);
+    }
+
+    static dotProduct(input1, input2) {
+        if(input1 instanceof Vector4f != true || input2 instanceof Vector4f != true){
+            throw "Parameters are not vectors!"
+        }
+
+        return input1.x*input2.x+input1.y*input2.y+input1.z*input2.z;
+    }
+
+    static crossProduct(input1, input2){
+        if(input1 instanceof Vector4f != true || input2 instanceof Vector4f != true){
+            throw "Parameters are not vectors!"
+        }
+
+        return new Vector4f(
+            input1.y*input2.z-input1.z*input2.y,
+            input1.z*input2.x-input1.x*input2.z,
+            input1.x*input2.y-input1.y*input2.x,
+            input1.h
+        );
+    }
+
+    static length(input){
+        if(input instanceof Vector4f != true){
+            throw "Parameters are not vectors!"
+        }
+
+        return Math.sqrt(input.x*input.x+input.y*input.y+input.z*input.z);
+    }
+
+    static normalize(input){
+        if(input instanceof Vector4f != true){
+            throw "Parameters are not vectors!"
+        }
+
+        var vectorLength = this.length(input);
+
+        if(vectorLength==0){
+            throw Error("Vector length is equal to 0. Cannot divide!");
+        }
+
+        return new Vector4f(
+            input.x/vectorLength,
+            input.y/vectorLength,
+            input.z/vectorLength
+        );
+    }
+
+    static project(input1, input2){
+        if(input1 instanceof Vector4f != true || input2 instanceof Vector4f != true){
+            throw "Parameters are not vectors!"
+        }
+
+        var input1Length = this.length(input1);
+
+        var dotProdukt = this.dotProduct(input1,input2);
+
+        if(input1Length == 0){
+            throw "Vector 1 is 0 units long. Cannot divide!"
+        }
+
+        var scalar = dotProdukt/(input1Length*input1Length);
+
+        return this.scalarProduct(scalar, input1);
+
+    }
+
+    static cosPhi(input1, input2){
+        if(input1 instanceof Vector4f != true || input2 instanceof Vector4f != true){
+            throw "Parameters are not vectors!"
+        }
+
+        var input1Length = this.length(input1);
+        var input2Length = this.length(input2);
+
+        var product = this.dotProduct(input1, input2);
+
+        if(input1Length*input2Length == 0){
+            throw ("Sum of vector lengths equals 0. Cannot divide!")
+        }
+
+        return product/(input1Length*input2Length);
+    }
+}
+
+module.exports = Vector4f;
+
---
+++ test/Vector4fTest.js
@@ -0,0 +1,237 @@
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const Vector4f = require('../Vector4f');
+const { dotProduct } = require('../Vector4f');
+
+describe('Vector4f class tests', function() {
+    //Constructor test
+
+    it('Vector object creation test', function() {
+        let vectorObj = new Vector4f(1,-2,3.14159,-4.213123423);
+
+        assert.instanceOf(vectorObj, Vector4f);
+        assert.equal(1, vectorObj.x);
+        assert.equal(-2, vectorObj.y);
+        assert.equal(3.14159, vectorObj.z);
+        assert.equal(-4.213123423, vectorObj.h);
+    });
+
+     //Negate function tests
+
+    it('Negate function test', function() {
+        let vectorObj = new Vector4f(4.2312, -2.12312, 5.744612, -14.2456452);
+        vectorObj = Vector4f.negate(vectorObj);
+
+        assert.instanceOf(vectorObj, Vector4f);
+        assert.equal(-4.2312, vectorObj.x);
+        assert.equal(2.12312, vectorObj.y);
+        assert.equal(-5.744612, vectorObj.z);
+        assert.equal(-14.2456452, vectorObj.h);
+    });
+
+    it('Negate function incorrect input test', function() {
+        expect(Vector4f.negate.bind("This is a test")).to.throw("Parameter is not a vector!");
+    });
+
+    //Add function tests
+
+    it('Add function test', function() {
+
+        let vectorObj1 = new Vector4f(4.2, -2, 5.7, -14.2);
+        let vectorObj2 = new Vector4f(1,-2,3.1,-4.2);
+
+        let vectorObj = Vector4f.add(vectorObj1, vectorObj2);
+
+        assert.instanceOf(vectorObj, Vector4f);
+        assert.equal(5.2, vectorObj.x);
+        assert.equal(-4, vectorObj.y);
+        assert.equal(8.8, vectorObj.z);
+        assert.equal(-14.2, vectorObj.h);
+    });
+
+    it('Add function incorrect input test', function() {
+
+        let vectorObj = new Vector4f(1,-2,3.1,-4.2);
+
+        expect(Vector4f.add.bind("This is a test", vectorObj)).to.throw("Parameters are not vectors!");
+        expect(Vector4f.add.bind(vectorObj, "If the function works")).to.throw("Parameters are not vectors!");
+    });
+
+    //ScalarProduct function tests
+
+    it('ScalarProduct function test', function() {
+
+        let vectorObj = new Vector4f(4.2, -2, 5.7, -14.2);
+        vectorObj = Vector4f.scalarProduct(2,vectorObj);
+
+        assert.instanceOf(vectorObj, Vector4f);
+        assert.equal(8.4, vectorObj.x);
+        assert.equal(-4, vectorObj.y);
+        assert.equal(11.4, vectorObj.z);
+        assert.equal(-14.2, vectorObj.h);
+
+        vectorObj = new Vector4f(1, 2, 3, 4);
+        vectorObj = Vector4f.scalarProduct(1.5,vectorObj);
+
+        assert.instanceOf(vectorObj, Vector4f);
+        assert.equal(1.5, vectorObj.x);
+        assert.equal(3, vectorObj.y);
+        assert.equal(4.5, vectorObj.z);
+        assert.equal(4, vectorObj.h);
+    });
+
+    it('ScalarProduct function incorrect input test', function() {
+
+        let vectorObj = new Vector4f(1,-2,3.1,-4.2);
+
+        expect(Vector4f.scalarProduct.bind("This is a test", vectorObj)).to.throw("Check parameter types!");
+        expect(Vector4f.scalarProduct.bind(3, "See if it fails")).to.throw("Check parameter types!");
+    });
+
+    //DotProduct function tests
+
+    it('DotProduct function test', function() {
+
+        let vectorObj1 = new Vector4f(4.2, -2, 5.7, -14.2);
+        let vectorObj2 = new Vector4f(1,-2,3.1,-4.2);
+        let result = Vector4f.dotProduct(vectorObj1,vectorObj2);
+
+        assert.equal(25.87, result);
+    });    
+
+    it('DotProduct incorrect input test', function() {
+
+        let vectorObj1 = new Vector4f(4.2, -2, 5.7, -14.2);
+        let vectorObj2 = new Vector4f(1,-2,3.1,-4.2);
+
+        expect(Vector4f.dotProduct.bind("This is a test", vectorObj2)).to.throw("Parameters are not vectors!");
+        expect(Vector4f.dotProduct.bind(vectorObj1, 3)).to.throw("Parameters are not vectors!");
+    });
+    
+  
+    //CrossProduct function tests
+
+    it('CrossProduct function test', function() {
+
+        let vectorObj1 = new Vector4f(4.2, -2, 5.7, -14.2);
+        let vectorObj2 = new Vector4f(1,-2,3.1,-4.2);
+        let result = Vector4f.crossProduct(vectorObj1,vectorObj2);
+
+        assert.instanceOf(result, Vector4f);
+        assert.equal(5.2, result.x);
+        assert.closeTo(-7.32, result.y, 0.0001);
+        assert.equal(-6.4, result.z);
+        assert.equal(-14.2, result.h);
+    });
+    
+    it('CrossProduct incorrect input test', function() {
+        let vectorObj1 = new Vector4f(4.2, -2, 5.7, -14.2);
+        let vectorObj2 = new Vector4f(1,-2,3.1,-4.2);
+
+        expect(Vector4f.crossProduct.bind("This is a test", vectorObj2)).to.throw("Parameters are not vectors!");
+        expect(Vector4f.crossProduct.bind(vectorObj1, undefined)).to.throw("Parameters are not vectors!");
+    });
+
+    //Length function tests
+
+    it('Length function test', function() {
+
+        let vectorObj = new Vector4f(6.4, -23.2, 6.1, -2);
+
+        let result = Vector4f.length(vectorObj);
+        
+        assert.equal(Math.sqrt((Math.pow(6.4,2)+Math.pow(-23.2,2)+Math.pow(6.1,2))), result);
+    });
+
+    it('Length incorrect input test', function() {
+        expect(Vector4f.length.bind("This is a test")).to.throw("Parameters are not vectors!");
+    });
+
+
+    //Normalize function tests
+
+    it('Normalize function test', function() {
+
+        let vectorObj1 = new Vector4f(6.4, -23.2, 6.1, -2);
+
+        let result = Vector4f.normalize(vectorObj1);
+
+        let vectorLength = Math.sqrt((Math.pow(6.4,2)+Math.pow(-23.2,2)+Math.pow(6.1,2)));
+        
+        assert.instanceOf(result, Vector4f);
+        assert.equal(6.4/vectorLength, result.x);
+        assert.equal(-23.2/vectorLength, result.y);
+        assert.equal(6.1/vectorLength, result.z);
+
+        let vectorObj2 = new Vector4f(0,0,0,0);
+
+        assert.throws(function() { Vector4f.normalize(vectorObj2) },"Vector length is equal to 0. Cannot divide!");
+
+    });
+
+    it('Normalize incorrect input test', function() {
+        expect(Vector4f.normalize.bind("This is a test")).to.throw("Parameters are not vectors!");
+    });
+
+    //Project function tests
+
+    it('Project function test', function() {
+
+        let vectorObj1 = new Vector4f(6, -23, 6, -2);
+        let vectorObj2 = new Vector4f(1, 4, 5, 3);
+
+        let vector1Length = Math.sqrt((Math.pow(6,2)+Math.pow(-23,2)+Math.pow(6,2)));
+        let dotProd = (6*1)+(-23*4)+(6*5);
+        let scalar = dotProd/(Math.pow(vector1Length,2));
+        
+        let result = Vector4f.project(vectorObj1, vectorObj2);
+
+        assert.instanceOf(result, Vector4f);
+        assert.equal(6*scalar, result.x);
+        assert.equal(-23*scalar, result.y);
+        assert.equal(6*scalar, result.z);
+        assert.equal(-2, result.h);
+
+        let vectorObj3 = new Vector4f(0,0,0,0);
+        assert.throws(function() { Vector4f.project(vectorObj3, vectorObj2) },"Vector 1 is 0 units long. Cannot divide!");
+
+    });
+
+    it('Project incorrect input test', function() {
+        let vectorObj1 = new Vector4f(4.2, -2, 5.7, -14.2);
+        let vectorObj2 = new Vector4f(1,-2,3.1,-4.2);
+
+        expect(Vector4f.project.bind("This is a test", vectorObj2)).to.throw("Parameters are not vectors!");
+        expect(Vector4f.project.bind(vectorObj1, undefined)).to.throw("Parameters are not vectors!");
+    });
+
+    //CosPhi function tests
+
+    it('CosPhi function test', function() {
+
+        let vectorObj1 = new Vector4f(6, -23, 6, -2);
+        let vectorObj2 = new Vector4f(1, 4, 5, 3);
+
+        let vector1Length = Math.sqrt((Math.pow(6,2)+Math.pow(-23,2)+Math.pow(6,2)));
+        let vector2Length = Math.sqrt((Math.pow(1,2)+Math.pow(4,2)+Math.pow(5,2)));
+
+        let dotProd = (6*1)+(-23*4)+(6*5);
+        let finalRes = dotProd/(vector1Length*vector2Length);
+        
+        let result = Vector4f.cosPhi(vectorObj1, vectorObj2);
+
+        assert.equal(finalRes, result);
+
+        let vectorObj3 = new Vector4f(0,0,0,0);
+        assert.throws(function() { Vector4f.cosPhi(vectorObj3, vectorObj2) },"Sum of vector lengths equals 0. Cannot divide!");
+
+    });
+
+    it('Project incorrect input test', function() {
+        let vectorObj1 = new Vector4f(4.2, -2, 5.7, -14.2);
+        let vectorObj2 = new Vector4f(1,-2,3.1,-4.2);
+
+        expect(Vector4f.cosPhi.bind("This is a test", vectorObj2)).to.throw("Parameters are not vectors!");
+        expect(Vector4f.cosPhi.bind(vectorObj1, undefined)).to.throw("Parameters are not vectors!");
+    });
+})
