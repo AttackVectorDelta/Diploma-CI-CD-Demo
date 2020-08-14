e8c0d6f6567b1c1e73b79bd2ef89e88a92fcf80b
AttackVectorDelta <nejc.ackun@gmail.com>
2020-08-15 00:07:20 +0200

    Added Matrix class and test suite

---
+++ js/Matrix4f.js
@@ -0,0 +1,107 @@
+class Matrix4f{
+    constructor(
+        x1,x2,x3,x4,
+        y1,y2,y3,y4,
+        z1,z2,z3,z4,
+        h1,h2,h3,h4
+    ){
+        this.x1=x1; this.x2=x2; this.x3=x3; this.x4=x4;
+
+        this.y1=y1; this.y2=y2; this.y3=y3; this.y4=y4;
+
+        this.z1=z1; this.z2=z2; this.z3=z3; this.z4=z4;
+
+        this.h1=h1; this.h2=h2; this.h3=h3; this.h4=h4;
+    }
+
+    stringify(){
+        return this.x1+" "+this.x2+" "+this.x3+" "+this.x4+"\n"+
+            this.y1+" "+this.y2+" "+this.y3+" "+this.y4+"\n"+
+            this.z1+" "+this.z2+" "+this.z3+" "+this.z4+"\n"+
+            this.h1+" "+this.h2+" "+this.h3+" "+this.h4;
+    }
+
+    static negate(input){
+        if(input instanceof Matrix4f != true){
+            throw "Input is not a matrix!"
+        }
+
+        return new Matrix4f(
+            -input.x1, -input.x2, -input.x3, -input.x4, 
+            -input.y1, -input.y2, -input.y3, -input.y4, 
+            -input.z1, -input.z2, -input.z3, -input.z4,
+            -input.h1, -input.h2, -input.h3, -input.h4
+        );
+    }
+
+    static add(input1, input2){
+        if(input1 instanceof Matrix4f != true || input2 instanceof Matrix4f != true){
+            throw "Input parameters are not matrices!"
+        }
+
+        return new Matrix4f(
+            input1.x1+input2.x1, input1.x2+input2.x2, input1.x3+input2.x3, input1.x4+input2.x4,
+            input1.y1+input2.y1, input1.y2+input2.y2, input1.y3+input2.y3, input1.y4+input2.y4,
+            input1.z1+input2.z1, input1.z2+input2.z2, input1.z3+input2.z3, input1.z4+input2.z4,
+            input1.h1+input2.h1, input1.h2+input2.h2, input1.h3+input2.h3, input1.h4+input2.h4
+        );
+    }
+
+    static transpose(input){
+        if(input instanceof Matrix4f != true){
+            throw "Input is not a matrix!"
+        }
+
+        return new Matrix4f(
+            input.x1, input.y1, input.z1, input.h1,
+            input.x2, input.y2, input.z2, input.h2,
+            input.x3, input.y3, input.z3, input.h3,
+            input.x4, input.y4, input.z4, input.h4
+        );
+    }
+
+    static multiplyScalar(input1, input2){
+        if(typeof input1 !="number" || input2 instanceof Matrix4f != true){
+            throw "Check input parameter types!"
+        }
+
+        return new Matrix4f(
+            input2.x1*input1, input2.x2*input1, input2.x3*input1, input2.x4*input1,
+            input2.y1*input1, input2.y2*input1, input2.y3*input1, input2.y4*input1,
+            input2.z1*input1, input2.z2*input1, input2.z3*input1, input2.z4*input1,
+            input2.h1*input1, input2.h2*input1, input2.h3*input1, input2.h4*input1
+        );
+
+    }
+
+    static multiply(input1, input2){
+        if(input1 instanceof Matrix4f != true || input2 instanceof Matrix4f != true){
+            throw "Input parameters are not matrices!"
+        }
+
+        return new Matrix4f(
+            input1.x1*input2.x1+input1.x2*input2.y1+input1.x3*input2.z1+input1.x4*input2.h1,
+            input1.x1*input2.x2+input1.x2*input2.y2+input1.x3*input2.z2+input1.x4*input2.h2,
+            input1.x1*input2.x3+input1.x2*input2.y3+input1.x3*input2.z3+input1.x4*input2.h3,
+            input1.x1*input2.x4+input1.x2*input2.y4+input1.x3*input2.z4+input1.x4*input2.h4,
+
+            input1.y1*input2.x1+input1.y2*input2.y1+input1.y3*input2.z1+input1.y4*input2.h1,
+            input1.y1*input2.x2+input1.y2*input2.y2+input1.y3*input2.z2+input1.y4*input2.h2,
+            input1.y1*input2.x3+input1.y2*input2.y3+input1.y3*input2.z3+input1.y4*input2.h3,
+            input1.y1*input2.x4+input1.y2*input2.y4+input1.y3*input2.z4+input1.y4*input2.h4,
+
+            input1.z1*input2.x1+input1.z2*input2.y1+input1.z3*input2.z1+input1.z4*input2.h1,
+            input1.z1*input2.x2+input1.z2*input2.y2+input1.z3*input2.z2+input1.z4*input2.h2,
+            input1.z1*input2.x3+input1.z2*input2.y3+input1.z3*input2.z3+input1.z4*input2.h3,
+            input1.z1*input2.x4+input1.z2*input2.y4+input1.z3*input2.z4+input1.z4*input2.h4,
+            
+            input1.h1*input2.x1+input1.h2*input2.y1+input1.h3*input2.z1+input1.h4*input2.h1,
+            input1.h1*input2.x2+input1.h2*input2.y2+input1.h3*input2.z2+input1.h4*input2.h2,
+            input1.h1*input2.x3+input1.h2*input2.y3+input1.h3*input2.z3+input1.h4*input2.h3,
+            input1.h1*input2.x4+input1.h2*input2.y4+input1.h3*input2.z4+input1.h4*input2.h4,
+        );
+    }
+
+}
+
+module.exports = Matrix4f;
---
+++ test/js/Matrix4fTest.js
@@ -0,0 +1,261 @@
+const assert = require('chai').assert;
+const Matrix4f = require('../../js/Matrix4f');
+
+describe('Matrix class tests', function() {
+
+    it('Matrix object creation test', function() {
+        let matrixObj = new Matrix4f(
+            1, 2.5, -3, -4.25,
+            -5, -6.5, 7, 8,
+            9, -10, 11, 12.25,
+            13.125, 14, -15.5, 16
+        );
+
+        assert.instanceOf(matrixObj, Matrix4f);
+        assert.equal(1, matrixObj.x1);
+        assert.equal(2.5, matrixObj.x2);
+        assert.equal(-3, matrixObj.x3);
+        assert.equal(-4.25, matrixObj.x4);
+
+        assert.equal(-5, matrixObj.y1);
+        assert.equal(-6.5, matrixObj.y2);
+        assert.equal(7, matrixObj.y3);
+        assert.equal(8, matrixObj.y4);
+
+        assert.equal(9, matrixObj.z1);
+        assert.equal(-10, matrixObj.z2);
+        assert.equal(11, matrixObj.z3);
+        assert.equal(12.25, matrixObj.z4);
+
+        assert.equal(13.125, matrixObj.h1);
+        assert.equal(14, matrixObj.h2);
+        assert.equal(-15.5, matrixObj.h3);
+        assert.equal(16, matrixObj.h4);
+    });
+
+    //Stringify function test
+
+    it('Matrix stringification test', function() {
+        let matrixObj = new Matrix4f(
+            1, 2.5, -3, -4.25,
+            -5, -6.5, 7, 8,
+            9, -10, 11, 12.25,
+            13.125, 14, -15.5, 16
+        );
+
+        let result = matrixObj.stringify();
+
+        let correctString = 
+        [1,2.5,-3,-4.25].join(" ")+"\n"+
+        [-5,-6.5,7,8].join(" ")+"\n"+
+        [9,-10,11,12.25].join(" ")+"\n"+
+        [13.125,14,-15.5,16].join(" ");
+
+        assert.equal(correctString, result);
+    });
+
+    //Add function tests
+
+    it('Add function test', function() {
+        let matrixObj1 = new Matrix4f(
+            1, 2.5, -3, -4.25,
+            -5, -6.5, 7, 8,
+            9, -10, 11, 12.25,
+            13.125, 14, -15.5, 16
+        );
+
+        let matrixObj2 = new Matrix4f(
+            1, 2, -3, 4,
+            5, 6, 7, 8,
+            9, 10, 11, 12,
+            13, 14, 15, 16
+        );
+
+        let result = Matrix4f.add(matrixObj1,matrixObj2);
+
+        assert.instanceOf(result, Matrix4f);
+        assert.equal(1+1, result.x1);
+        assert.equal(2.5+2, result.x2);
+        assert.equal(-3+-3, result.x3);
+        assert.equal(-4.25+4, result.x4);
+
+        assert.equal(-5+5, result.y1);
+        assert.equal(-6.5+6, result.y2);
+        assert.equal(7+7, result.y3);
+        assert.equal(8+8, result.y4);
+
+        assert.equal(9+9, result.z1);
+        assert.equal(-10+10, result.z2);
+        assert.equal(11+11, result.z3);
+        assert.equal(12.25+12, result.z4);
+
+        assert.equal(13.125+13, result.h1);
+        assert.equal(14+14, result.h2);
+        assert.equal(-15.5+15, result.h3);
+        assert.equal(16+16, result.h4);
+    });
+
+    it('Add incorrect input test', function() {
+        let matrixObj1 = new Matrix4f(
+            1, 2.5, -3, -4.25,
+            -5, -6.5, 7, 8,
+            9, -10, 11, 12.25,
+            13.125, 14, -15.5, 16
+        );
+
+        let matrixObj2 = new Matrix4f(
+            1, 2, -3, 4,
+            5, 6, 7, 8,
+            9, 10, 11, 12,
+            13, 14, 15, 16
+        );
+        
+        assert.throws(function() {Matrix4f.add("This is a test", matrixObj1)},"Input parameters are not matrices!");
+        assert.throws(function() {Matrix4f.add(matrixObj2, "If the function works")},"Input parameters are not matrices!");
+    });
+
+    //Transpose function tests
+
+    it('Transpose function test', function() {
+        let matrixObj = new Matrix4f(
+            1, 2.5, -3, -4.25,
+            -5, -6.5, 7, 8,
+            9, -10, 11, 12.25,
+            13.125, 14, -15.5, 16
+        );
+
+        let result = Matrix4f.transpose(matrixObj);
+
+        assert.instanceOf(result, Matrix4f);
+        assert.equal(1, result.x1);
+        assert.equal(-5, result.x2);
+        assert.equal(9, result.x3);
+        assert.equal(13.125, result.x4);
+
+        assert.equal(2.5, result.y1);
+        assert.equal(-6.5, result.y2);
+        assert.equal(-10, result.y3);
+        assert.equal(14, result.y4);
+
+        assert.equal(-3, result.z1);
+        assert.equal(7, result.z2);
+        assert.equal(11, result.z3);
+        assert.equal(-15.5, result.z4);
+
+        assert.equal(-4.25, result.h1);
+        assert.equal(8, result.h2);
+        assert.equal(12.25, result.h3);
+        assert.equal(16, result.h4);
+    });
+
+    it('Transpose incorrect input test', function() {
+        assert.throws(function() {Matrix4f.transpose("This is a test")},"Input is not a matrix!");
+    });
+
+    //MultiplyScalar function tests
+
+    it('MultiplyScalar function test', function() {
+        let matrixObj = new Matrix4f(
+            1, 2.5, -3, -4.25,
+            -5, -6.5, 7, 8,
+            9, -10, 11, 12.25,
+            13.125, 14, -15.5, 16
+        );
+
+        let result = Matrix4f.multiplyScalar(2, matrixObj);
+
+        assert.instanceOf(result, Matrix4f);
+        assert.equal(1*2, result.x1);
+        assert.equal(2.5*2, result.x2);
+        assert.equal(-3*2, result.x3);
+        assert.equal(-4.25*2, result.x4);
+
+        assert.equal(-5*2, result.y1);
+        assert.equal(-6.5*2, result.y2);
+        assert.equal(7*2, result.y3);
+        assert.equal(8*2, result.y4);
+
+        assert.equal(9*2, result.z1);
+        assert.equal(-10*2, result.z2);
+        assert.equal(11*2, result.z3);
+        assert.equal(12.25*2, result.z4);
+
+        assert.equal(13.125*2, result.h1);
+        assert.equal(14*2, result.h2);
+        assert.equal(-15.5*2, result.h3);
+        assert.equal(16*2, result.h4);
+    });
+
+    it('MultiplyScalar incorrect input test', function() {
+        let matrixObj = new Matrix4f(
+            1, 2.5, -3, -4.25,
+            -5, -6.5, 7, 8,
+            9, -10, 11, 12.25,
+            13.125, 14, -15.5, 16
+        );
+
+        assert.throws(function() {Matrix4f.multiplyScalar("This is a test", matrixObj)},"Check input parameter types!");
+        assert.throws(function() {Matrix4f.multiplyScalar(2, undefined)},"Check input parameter types!");
+    });
+
+    //Multiplyfunction tests
+
+    it('Multiply function test', function() {
+        let matrixObj1 = new Matrix4f(
+            4, 6, 3, 4,
+            5, 12, -7, 8,
+            9, -15, 13, 1,
+            16, 11, 5, 7
+        );
+
+        let matrixObj2 = new Matrix4f(
+            1, 2, -3, 4,
+            5, 6, 7, 8,
+            9, 10, 11, 12,
+            13, 14, 15, 16
+        );
+
+        let result = Matrix4f.multiply(matrixObj1, matrixObj2);
+
+        assert.instanceOf(result, Matrix4f);
+        assert.equal(4*1+6*5+3*9+4*13, result.x1);
+        assert.equal(4*2+6*6+3*10+4*14, result.x2);
+        assert.equal((4*-3)+6*7+3*11+4*15, result.x3);
+        assert.equal(4*4+6*8+3*12+4*16, result.x4);
+
+        assert.equal(5*1+12*5+(-7*9)+8*13, result.y1);
+        assert.equal(5*2+12*6+(-7*10)+8*14, result.y2);
+        assert.equal((5*-3)+12*7+(-7*11)+8*15, result.y3);
+        assert.equal(5*4+12*8+(-7*12)+8*16, result.y4);
+
+        assert.equal(9*1+(-15*5)+13*9+1*13, result.z1);
+        assert.equal(9*2+(-15*6)+13*10+1*14, result.z2);
+        assert.equal((9*-3)+(-15*7)+13*11+1*15, result.z3);
+        assert.equal(9*4+(-15*8)+13*12+1*16, result.z4);
+
+        assert.equal(16*1+11*5+5*9+7*13, result.h1);
+        assert.equal(16*2+11*6+5*10+7*14, result.h2);
+        assert.equal((16*-3)+11*7+5*11+7*15, result.h3);
+        assert.equal(16*4+11*8+5*12+7*16, result.h4);
+    });
+
+    it('MultiplyScalar incorrect input test', function() {
+        let matrixObj1 = new Matrix4f(
+            4, 6, 3, 4,
+            5, 12, -7, 8,
+            9, -15, 13, 1,
+            16, 11, 5, 7
+        );
+
+        let matrixObj2 = new Matrix4f(
+            1, 2, -3, 4,
+            5, 6, 7, 8,
+            9, 10, 11, 12,
+            13, 14, 15, 16
+        );
+
+        assert.throws(function() {Matrix4f.multiply("This is a test", matrixObj1)},"Input parameters are not matrices!");
+        assert.throws(function() {Matrix4f.multiply(matrixObj2, undefined)},"Input parameters are not matrices!");
+    });
+
+});
